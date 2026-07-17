6b451eca5630e0e56900687940745497ac98cf7c
Pachito_27 <francisco@geddes.com.ar>
2026-07-16 21:08:23 -0300

    arregle el cambio de escena y añadi el personaje

--- ProgGraf1/Assets/Deliver/ProyectoIntegrador/Shaders-FranciscoGeddes/CambioEscena.cs
+++ ProgGraf1/Assets/Deliver/ProyectoIntegrador/Shaders-FranciscoGeddes/CambioEscena.cs
@@ -2,18 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+   
 
 public class CambioEscena : MonoBehaviour
 {
-    [SerializeField] private string PI_01;
-
-    // Se ejecuta cuando un objeto con Collider entra en el Trigger
     private void OnTriggerEnter(Collider other)
     {
-        // Comparamos si el objeto que entró tiene la etiqueta "Player"
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(PI_01);
+            // Reemplaza "NombreDeTuEscena" por el nombre exacto de tu escena
+            SceneManager.LoadScene("PI_01");
         }
     }
 }
